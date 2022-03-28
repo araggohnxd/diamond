9046d4f22c92ffbd52cf3441b20f8fd44fca52ba
araggohnxd <matheusnagato300@gmail.com>
2022-03-28 11:52:56 -0300

    Remove useless define

--- ft_diamond.h
+++ ft_diamond.h
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:43:43 by maolivei          #+#    #+#             */
-/*   Updated: 2022/03/27 21:03:28 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/03/28 11:49:41 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,5 @@ typedef struct s_struct
 	int	current;
 	int	increment;
 }	t_struct;
-
-# define TRUE 1
 
 #endif
